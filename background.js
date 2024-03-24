01b790e9858bb6b49402ca8017d8baf1cc33ffb8
bcblevins <beaucblevins@gmail.com>
2024-03-24 08:45:59 -0400

    added comments and sources

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-
+//[1]
 //chrome.contextMenus: API provided by chrome to allow adding to context (right click) menu in chrome
 chrome.contextMenus.create({
 
@@ -13,27 +13,60 @@ chrome.contextMenus.create({
     id: 'dateStamp'
 });
 
-
+//[1]
 //onClicked: this is an event that triggers when you click something, in this case: the context menu
 //addListener: this is something that will watch out for the 'onClicked' event and call the function
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
     //check if the item that was clicked was the menu item we created
     if (info.menuItemId === "dateStamp") {
-
-        //TODO: figure this one out
+        
+        //[2]
+        //chrome.scripting: an API that allows scripts to be injected into websites.
+        //executeScript: does the above, specificaly from a function rather than a file.
         chrome.scripting.executeScript({
+
+            //selects where the script should be injected to and what functino to call
             target: { tabId: tab.id },
-            function: insertTextFunction
+            function: insertTimeStamp
         });
     }
 });
 
+//function that is called above
+function insertTimeStamp() {
 
-function insertTextFunction() {
-    let now = new Date()
+    //[3],[4]
+    //document.activeElement: returns the element in the DOM that currently has focus by the user. 
+    //DOM: data representation of the structure of a webpage that acts as an interface for programs to interact with the webpage
     var activeElement = document.activeElement;
+
+    //[5]
+    //determines if the activeElement is something that we can inject text into.
     if (activeElement.tagName.toLowerCase() === "input" || activeElement.tagName.toLowerCase() === "textarea") {
+
+        //adds date to the end of the selected editable element
+        //TODO: make the text insert at the cursor instead of at the end of the editable element
+        let now = new Date()
         activeElement.value += now.toLocaleString();
     }
 }
+
+
+
+//SOURCES FOR NOTES:
+
+//[1] Context Menus:
+//https://developer.chrome.com/docs/extensions/reference/api/contextMenus
+
+//[2] Scripting:
+//https://developer.chrome.com/docs/extensions/reference/api/scripting
+
+//[3] DOM:
+//https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
+
+//[4] document.activeElement:
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/activeElement
+
+//[5] activeElement.tagName:
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/tagName
