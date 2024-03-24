d8b15f0591fcd56a3caaefe2774da8403f1cf7fc
bcblevins <beaucblevins@gmail.com>
2024-03-24 07:54:42 -0400

    Extension now working! Adding comments

--- background.js
+++ background.js
@@ -1,24 +1,39 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        title: 'Insert Date Stamp',  //this will show up in the context menu
-        contexts: ['editable'],      //this is used to tell when our context menu item should be displayed, I believe editable will restrict it to text boxes and the like
-        id: 'dateStamp'
-    });
-})
 
-function timestamp() {
-    console.log('DateStamp!!')
+//chrome.contextMenus: API provided by chrome to allow adding to context (right click) menu in chrome
+chrome.contextMenus.create({
+
+    //'title' will show up in the context menu
+    title: 'Insert Date Stamp',
+
+    //'contexts' is used to tell when our context menu item should be displayed, 
+    //editable will restrict it to text boxes and the like
+    contexts: ['editable'],
+    
+    //'id' is used to refer to our menu item in our code.
+    id: 'dateStamp'
+});
+
+
+//onClicked: this is an event that triggers when you click something, in this case: the context menu
+//addListener: this is something that will watch out for the 'onClicked' event and call the function
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+
+    //check if the item that was clicked was the menu item we created
+    if (info.menuItemId === "dateStamp") {
+
+        //TODO: figure this one out
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: insertTextFunction
+        });
+    }
+});
+
+
+function insertTextFunction() {
+    let now = new Date()
+    var activeElement = document.activeElement;
+    if (activeElement.tagName.toLowerCase() === "input" || activeElement.tagName.toLowerCase() === "textarea") {
+        activeElement.value += now.toLocaleString();
+    }
 }
-
-chrome.contextMenus.onClicked.addListener(timestamp)
-/*
-The above broken down:
-    chrome: the global object provided by chrome's extension system
-    contextMenus: this is a property of 'chrome' that represents the context menu API we will be using to add our 
-        extension's capabilities to the right click menu
-    onClicked: this is an event that triggers when you click something, in our case: our context menu button
-    addListener: this is something that will watch out for the 'onClicked' event and call the function
-*/
-
-
-
--- script.js
+++
@@ -1,5 +0,0 @@
-console.log('worked?', 'hello?')
-
-let date = new Date()
-
-document.getElementById("date").innerHTML = date.toLocaleString();
